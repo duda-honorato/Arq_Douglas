745f1e6cb93a6d8467d484698129b5cc2d08ea69
HDuda99 <111542543+duda-honorato@users.noreply.github.com>
2025-11-14 00:01:34 -0300

    Update Paralelo.py

--- Paralelo.py
+++ Paralelo.py
@@ -6,9 +6,19 @@ import random
 import time
 import math
 
-def pi_paralelo(n_pontos):
+def pi_parcial(n_pontos):
   pontos_dentro = 0
   pontos_fora = 0
-  #Incluir Seed para meio que separar os processos, sem isso tera processos duplicados
+  #Incluir Seed para meio que separar os processos, sem isso tera processos duplicados - DIVISAO DE PROCESSOS
   random.seed(MPI.COMM_WORLD.Get_rank() + int(time.time()))
-  for_in rab
+  for i in range(n_pontos):
+        x = random.random()
+        y = random.random()
+        z = x*x + y*y
+        if z <= 1.0:
+            pontos_dentro += 1
+        else
+            pontos_fora += 1
+  return pontos_dentro, pontos_fora
+def main():
+
