5a2ca84d73db296d0b98a4ed16511530c8437577
HDuda99 <111542543+duda-honorato@users.noreply.github.com>
2025-11-22 15:11:38 -0300

    Update Paralelo (N INICIADO).py

--- Paralelo (N INICIADO).py
+++ Paralelo (N INICIADO).py
@@ -1,1 +1,13 @@
-#cdg -- N INICIADO
+import random 
+import time
+import math
+from mpi4py imoport MPI
+
+def pi_paralelo(n_pontos):
+  #Iniciliazar
+  comm = MPI.COMM_WORLD
+  rank = comm.Get_rank()
+  size = comm.Get_size()
+  #Divisao de Processos -- Pontos/Processo
+  
+
